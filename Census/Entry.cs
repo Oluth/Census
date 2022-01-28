fb5d560d8f7ad91e7856220e81f566b8b14008fd
Till-André Diegeler <t.diegeler@ostfalia.de>
2022-01-29 00:58:36 +0100

    Add primitive in-game debug support.

--- Census/Entry.cs
+++ Census/Entry.cs
@@ -1,4 +1,6 @@
 ﻿using ICities;
+using Census.Service;
+using Census.Service.Debug;
 
 namespace Census
 {
@@ -6,10 +8,31 @@ namespace Census
     /// <summary>
     /// This is the entry point of <b>Census</b> once it's loaded in game. 
     /// </summary>
-    public class Entry : IUserMod
+    public sealed class Entry : IUserMod, ILoadingExtension
     {
         string IUserMod.Name => "Census";
 
         string IUserMod.Description => "[Pre-Alpha] A Cities: Skylines demography tool.";
+
+        // INFO - DEBUG OUTPUTS ARE PLACEHOLDERS!
+        public void OnCreated(ILoading loading)
+        {
+            DebugService.Log(DebugState.error, "Mod created!");
+        }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            DebugService.Log(DebugState.warning, "Level loaded!");
+        }
+
+        public void OnLevelUnloading()
+        {
+            DebugService.Log(DebugState.warning, "Level unloaded!");
+        }
+
+        public void OnReleased()
+        {
+            DebugService.Log(DebugState.info, "Level released!");
+        }
     }
 }
---
+++ Census/Service/DebugService.cs
@@ -0,0 +1,32 @@
+﻿using Census.Service.Debug;
+using System;
+using UnityEngine;
+
+namespace Census.Service
+{
+    /// <summary>
+    /// Service providing the interfaces to handle debug messages from the mod.
+    /// </summary>
+    internal static class DebugService
+    {
+
+        internal static void Log(DebugState state, string msg)
+        {
+            switch(state)
+            {
+                case DebugState.error:
+                    UnityEngine.Debug.LogError(msg);
+                    break;
+                case DebugState.warning:
+                    UnityEngine.Debug.LogWarning(msg);
+                    break;
+                case DebugState.info:
+                    UnityEngine.Debug.Log(msg);
+                    break;
+                default:
+                    UnityEngine.Debug.Log(msg);
+                    break;
+            }
+        }
+    }
+}
